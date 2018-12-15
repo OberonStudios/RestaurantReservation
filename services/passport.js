912fe9f2f62c199a2d840730ea84089b5e6cdcce
Angel Contreras <angelcbenoit@gmail.com>
2018-12-15 08:13:07 -0800

    init files

---
+++ services/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const key = require('../config/key.js');
+const mongoose = require('mongoose');
+const User = mongoose.model('user');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    })
+})
+
+
+passport.use(new GoogleStrategy({
+    clientID: key.googleClientID,
+    clientSecret: key.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+}, (accessToken, refreshToken, profile, done) => {
+
+    User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+            if (existingUser) {
+                done(null, existingUser);
+            } else { //create new user if existingUser === false
+                new User({ googleId: profile.id }).save()
+                    .then((user) => done(null, user));
+            }
+        })
+
+})
+);
