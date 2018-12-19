83db2037217573b8cbbe5adc54eaa87f451fd8e7
Angel Contreras <angelcbenoit@gmail.com>
2018-12-19 12:22:26 -0800

    redesign the landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import Salad from '../images/salad.jpg';
+import Pancake from '../images/pancakes.jpg';
 
 export default class Landing extends Component {
   render() {
     return (
       <div className="homePage">
-        <Navbar/>
-        <div className="homePage__text">
+        <Navbar />
+
+        <div className="homePage__images">
+          <div className="homePage__images__title">
             <h3>Oberon Lunch House</h3>
+            <p>Make your reservation today.</p>
+
+            <img src={Pancake}/>
+          </div>
+
+          <img src={Salad}/>
+
         </div>
       </div>
     )
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,16 +5,16 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
-        <NavLink activeStyle={{color: "green"}}  to="/">
+        <NavLink activeStyle={{color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))"}}  to="/">
         <i class="fas fa-home"></i> Home
         </NavLink>
-        <NavLink activeStyle={{color: "green"}} to="/menu">
+        <NavLink activeStyle={{backgroundColor: "silver"}} to="/menu">
         <i class="fas fa-utensils"></i> Menu
         </NavLink>
-        <NavLink activeStyle={{color: "green"}}  to="/reservation">
+        <NavLink activeStyle={{backgroundColor: "silver"}}  to="/reservation">
         <i class="far fa-check-square"></i> Make Reservation
         </NavLink>
-        <NavLink activeStyle={{color: "green"}}  to="/about">
+        <NavLink activeStyle={{backgroundColor: "silver"}}  to="/about">
         <i class="fas fa-info-circle"></i> About Us
         </NavLink>
       </div>
