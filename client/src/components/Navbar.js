bd738a4c490bc847c62971c75618222dd68b9f47
Angel Contreras <angelcbenoit@gmail.com>
2019-01-09 14:22:29 -0800

    update navbar to include appropriate links depending whether user is logged in or not

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default class Navbar extends Component {
+class Navbar extends Component {
+  //if user is not logged in, return login link
+  renderAuthentication() {
+    if (!this.props.auth) {
+      return (
+        <>
+          <a activeStyle={{ color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))" }} href="/auth/google" exact>
+            <i className="fas fa-home"></i> Login
+          </a>
+        </>
+      );
+    }
+    //otherwise return a link to user's dashboard, and option to logout
+    else {
+      return (
+        <>
+          <NavLink activeStyle={{ color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))" }} to="/reservation" exact>
+            <i className="far fa-check-square"></i> Make Reservation
+          </NavLink>
+          <NavLink activeStyle={{ color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))" }} to="/dashboard" exact>
+            <i className="fas fa-home"></i> Dashboard
+          </NavLink>
+          <a activeStyle={{ color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))" }} href="/api/logout" exact>
+            <i className="fas fa-home"></i> Logout
+          </a>
+        </>
+      )
+    }
+  }
   render() {
     return (
       <div className="navbar">
-        <NavLink activeStyle={{color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))"}}  to="/" exact>
-        <i className="fas fa-home"></i> Home
+        <NavLink activeStyle={{ color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))" }} to="/" exact>
+          <i className="fas fa-home"></i> Home
         </NavLink>
-        <NavLink activeStyle={{color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))"}} to="/menu" exact>
-        <i className="fas fa-utensils"></i> Menu
+        <NavLink activeStyle={{ color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))" }} to="/menu" exact>
+          <i className="fas fa-utensils"></i> Menu
         </NavLink>
-        <NavLink activeStyle={{color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))"}}  to="/reservation" exact>
-        <i className="far fa-check-square"></i> Make Reservation
+        <NavLink activeStyle={{ color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))" }} to="/about" exact>
+          <i className="fas fa-info-circle"></i> About Us
         </NavLink>
-        <NavLink activeStyle={{color: "white", background: "linear-gradient(to top left, rgb(255, 29, 86), rgb(218, 65, 111))"}}  to="/about" exact>
-        <i className="fas fa-info-circle"></i> About Us
-        </NavLink>
+        {this.renderAuthentication()}
       </div>
-    )
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(Navbar);
